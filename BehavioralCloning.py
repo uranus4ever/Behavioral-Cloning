b45aa94e689a2b874875cfcf771207a1ac36f083
Chaoqun Shan <shanchaoqun27@163.com>
2017-10-07 15:24:20 +0800

    update generator
    compile sucess
    simulate error: shape(None, 64, 128, 3) not match predict (1, 160, 320, 3)

--- BehavioralCloning.py
+++ BehavioralCloning.py
@@ -1,15 +1,17 @@
 import csv
 import cv2
 import numpy as np
-import pickle
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Activation, Cropping2D
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
-from keras.models import Model
+from keras.optimizers import Adam
 import matplotlib.pyplot as plt
 from scipy.stats import bernoulli
 import scipy.misc
+import math
+from sklearn.utils import shuffle
+import matplotlib.image as mpimg
 
 
 def path(source_path):
@@ -19,7 +21,6 @@ def path(source_path):
 
 
 def random_flip(image, steering_angle, flipping_prob=0.5):
-
     head = bernoulli.rvs(flipping_prob)
     if head:
         return np.fliplr(image), -1 * steering_angle
@@ -33,12 +34,17 @@ def crop_img(image):
     return image[top:bottom, :]
 
 
+def blur(img):
+    gb = cv2.GaussianBlur(img, (5,5), 20.0)
+    return cv2.addWeighted(img, 2, gb, -1, 0)
+
 def resize(image, new_size):
     return scipy.misc.imresize(image, new_size)
 
 
 def process_img(image, steering_angle):
     image = crop_img(image)
+    image = blur(image)
     image, steering_angle = random_flip(image, steering_angle, flipping_prob=0.5)
     image = resize(image, new_size=(64, 128))
     return image, steering_angle
@@ -47,115 +53,145 @@ def process_img(image, steering_angle):
 def select_img(line):
     indicator_img = np.random.randint(0, 3)
     correction = 0.2
-    if indicator_img == 0: # center
+    if indicator_img == 0:  # center
         img = cv2.imread(path(line[0]))
         angle = float(line[3])
-    elif indicator_img == 1: # left
+    elif indicator_img == 1:  # left
         img = cv2.imread(path(line[1]))
         angle = float(line[3]) + correction
-    else: # right
+    else:  # right
         img = cv2.imread(path(line[2]))
         angle = float(line[3]) - correction
     return img, angle
 
 
-def load_data(lines, batch_size=64):
+def visualize(lines):
+    # three camera view
+    plt.figure(figsize=(12,4))
+    imtitle1 = ['left', 'center', 'right']
+    p = [path(lines[0][1]), path(lines[0][0]), path(lines[0][2])]
+    for i in range(3):
+        img = mpimg.imread(p[i])
+        plt.subplot(1, 3, i+1)
+        plt.imshow(img)
+        plt.axis('off')
+        plt.title(imtitle1[i])
+    plt.show()
+
+    # image process
+    img = mpimg.imread(p[1])
+    crop = crop_img(img)
+    bl = blur(crop)
+    flip = np.fliplr(bl)
+    resz = resize(flip, (64,128))
+    imgs = [img, crop, bl, flip, resz]
+
+    plt.figure(figsize=(16, 6))
+    imtitle2 = ['Raw', 'Crop', 'Sharpen', 'Flip', 'Resize']
+    for i in range(5):
+        plt.subplot(1, 5, i+1)
+        plt.imshow(imgs[i])
+        plt.axis('off')
+        plt.title(imtitle2[i])
+    plt.show()
+
+def load_data(lines, batch_size):
     while True:
-        X_batch = []
+        x_batch = []
         y_batch = []
-        batch = 0
-        if (batch + batch_size) < len(lines):
-            for line in lines[batch:batch+batch_size]:
-                image, angle = select_img(line)
-                image_process, angle_process = process_img(image, angle)
-                X_batch.append(image_process)
-                y_batch.append(angle_process)
-
-            batch += batch_size
-        yield np.array(X_batch), np.array(y_batch)
-
-data_saved = False
-if data_saved:
-    print('Loading data...')
-    with open('./data_pickle.p', mode="rb") as f:
-        data_pickle = pickle.load(f)
-    num_data = len(data_pickle["y_data"])
-    train_gen = np.array(data_pickle["x_data"][: int(0.8*num_data)],
-                         data_pickle["y_data"][: int(0.8*num_data)])
-    valid_gen = np.array(data_pickle["x_data"][int(0.8*num_data):],
-                         data_pickle["y_data"][int(0.8*num_data):])
-else:
-    lines = []
-    with open('./data/driving_log.csv') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            lines.append(row)
-    num_data = len(lines)
-    print('num_data = {}'.format(num_data))
-    batch_size = 64
-    generator = load_data(lines, batch_size)
-    data = []
-    #
-    for i in range(int(num_data / batch_size)):
-        data.append(next(generator))
-
-    new = []
-    for idx in range(64):
-        new[0][idx] = data[0][0][idx]
-        new[1][idx] = data[0][1][idx]
+        idx = 0
+        for line in lines:
+            image, angle = select_img(line)
+            image_process, angle_process = process_img(image, angle)
+            x_batch.append(image_process)
+            y_batch.append(angle_process)
+            idx += 1
+            if idx == batch_size:
+                idx = 0
+                yield np.array(x_batch), np.array(y_batch)
+                x_batch = []
+                y_batch = []
 
 
+lines = []
+with open('./data/driving_log.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        lines.append(row)
+num_data = len(lines)
+lines = shuffle(lines)
+print('num_data = {}'.format(num_data))
 
 
-    # train_gen = dataset[:, :int(0.8*len(dataset[1]))]
-    # valid_gen = dataset[:, int(0.8*len(dataset[1])):]
+model = Sequential()
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(64, 128, 3)))
 
-    # save data into pickle file
-    # data_pickle = {}
-    # data_pickle['x_data'] = X_batch
-    # data_pickle['y_data'] = y_batch
-    # pickle.dump(data_pickle, open("./data_pickle.p", "wb"))
-    # print('Data saved')
+model.add(Convolution2D(24, 5, 5, border_mode='same', subsample=(2, 2)))
+model.add(Activation(activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
 
-#
-# model = Sequential()
-# model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
-#
-# model.add(Convolution2D(6,5,5,activation="relu"))
-# model.add(MaxPooling2D())
-# model.add(Convolution2D(6,5,5,activation="relu"))
-# model.add(MaxPooling2D())
-# model.add(Flatten())
-# model.add(Dense(120))
-# model.add(Dense(84))
-# model.add(Dense(1))
-#
-# model.compile(loss='mse', optimizer='adam')
-# model.fit(X_train, y_train, validation_split=0.2, shuffle=True,
-#           nb_epoch=3)
-#
-#
-#
-# model.save('model.h5')
-# print("Model Training Completed.")
-#
-# history = model.fit_generator(train_gen,
-#                               samples_per_epoch=number_of_samples_per_epoch,
-#                               nb_epoch=number_of_epochs,
-#                               validation_data=validation_gen,
-#                               nb_val_samples=number_of_validation_samples,
-#                               verbose=1)
-#
-#
-#
-# ### print the keys contained in the history object
-# print(history.history.keys())
-#
+model.add(Convolution2D(36, 5, 5, border_mode='same', subsample=(2, 2)))
+model.add(Activation(activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+model.add(Convolution2D(48, 5, 5, border_mode='same', subsample=(2, 2)))
+model.add(Activation(activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
+model.add(Activation(activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+model.add(Convolution2D(64, 3, 3, border_mode='same', subsample=(1, 1)))
+model.add(Activation(activation="relu"))
+model.add(MaxPooling2D(pool_size=(2, 2), strides=(1, 1)))
+
+model.add(Flatten())
+
+# Next, five fully connected layers
+model.add(Dense(1164))
+model.add(Activation(activation="relu"))
+
+model.add(Dense(100))
+model.add(Activation(activation="relu"))
+
+model.add(Dense(50))
+model.add(Activation(activation="relu"))
+
+model.add(Dense(10))
+model.add(Activation(activation="relu"))
+
+model.add(Dense(1))
+
+model.summary()
+
+batch_size = 128
+learning_rate = 1e-4
+
+model.compile(optimizer=Adam(learning_rate), loss="mse")
+
+training_num = math.ceil(num_data * 0.8/batch_size)*batch_size
+validation_num = math.ceil(num_data * 0.2/batch_size)*batch_size
+result = model.fit_generator(generator=load_data(lines[:training_num], batch_size),
+                             samples_per_epoch=training_num,
+                             nb_epoch=3,
+                             validation_data=load_data(lines[-validation_num:], batch_size),
+                             nb_val_samples=validation_num,
+                             verbose=1)
+
+# ## print the keys contained in the history object
+# print(result.history.keys())
+model.save('model.h5')
+print("Model Saved.")
+
 # ### plot the training and validation loss for each epoch
-# plt.plot(history.history['loss'])
-# plt.plot(history.history['val_loss'])
-# plt.title('model mean squared error loss')
-# plt.ylabel('mean squared error loss')
-# plt.xlabel('epoch')
-# plt.legend(['training set', 'validation set'], loc='upper right')
-# plt.show()
+plt.figure()
+plt.plot(result.epoch, result.history['loss'])
+plt.plot(result.epoch, result.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.ylim([0, 0.1])
+plt.show()
+
